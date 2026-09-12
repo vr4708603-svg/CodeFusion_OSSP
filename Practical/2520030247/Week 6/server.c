0c3bda6334d3e71d9bea6d53150bc278e5e45104
vr4708603-svg <vr4708603@gmail.com>
2026-09-12 08:55:16 +0530

    Implement server FIFO communication in server.c

--- Practical/2520030247/Week 6/server.c
+++ Practical/2520030247/Week 6/server.c
@@ -1,1 +1,41 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <string.h>
 
+int main()
+{
+    int fd1, fd2;
+    char buffer[100];
+    char response[100];
+
+    mkfifo("client_fifo", 0666);
+    mkfifo("server_fifo", 0666);
+
+    fd1 = open("client_fifo", O_RDONLY);
+    fd2 = open("server_fifo", O_WRONLY);
+
+    if (fd1 == -1 || fd2 == -1)
+    {
+        perror("open");
+        exit(1);
+    }
+
+    read(fd1, buffer, sizeof(buffer));
+
+    printf("Server received: %s\n", buffer);
+
+    strcpy(response, "Message processed by server");
+
+    write(fd2, response, strlen(response) + 1);
+
+    close(fd1);
+    close(fd2);
+
+    unlink("client_fifo");
+    unlink("server_fifo");
+
+    return 0;
+}
