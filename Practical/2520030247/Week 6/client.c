ec3ee78e7577f1f58e484b817e8bf0cd870ebba4
vr4708603-svg <vr4708603@gmail.com>
2026-09-12 08:54:45 +0530

    Update client.c

--- Practical/2520030247/Week 6/client.c
+++ Practical/2520030247/Week 6/client.c
@@ -1,1 +1,34 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
 
+int main()
+{
+    int fd1, fd2;
+    char message[] = "Hello Server";
+    char buffer[100];
+
+    fd1 = open("client_fifo", O_WRONLY);
+    fd2 = open("server_fifo", O_RDONLY);
+
+    if (fd1 == -1 || fd2 == -1)
+    {
+        perror("open");
+        exit(1);
+    }
+
+    write(fd1, message, strlen(message) + 1);
+
+    printf("Client sent: %s\n", message);
+
+    read(fd2, buffer, sizeof(buffer));
+
+    printf("Server response: %s\n", buffer);
+
+    close(fd1);
+    close(fd2);
+
+    return 0;
+}
