b77f486379f61c38c19f74da3de2ad4d9d71e02c
vr4708603-svg <vr4708603@gmail.com>
2026-09-12 08:50:39 +0530

    Implement multi-process handling with wait in C
    This program creates three child processes that sleep for different durations before printing a message. The parent process waits for each child to finish before printing a final message.

--- Practical/2520030247/Week 4/Week4.c
+++ Practical/2520030247/Week 4/Week4.c
@@ -1,1 +1,46 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/wait.h>
 
+int main()
+{
+    pid_t c1, c2, c3;
+
+    c1 = fork();
+
+    if(c1 == 0){
+        sleep(3);
+        printf("Child 1 finished\n");
+        exit(0);
+    }
+
+    c2 = fork();
+
+    if(c2 == 0){
+        sleep(2);
+        printf("Child 2 finished\n");
+        exit(0);
+    }
+
+    c3 = fork();
+
+    if(c3 == 0){
+        sleep(3);
+        printf("Child 3 finished\n");
+        exit(0);
+    }
+
+    printf("Parent waiting for child\n");
+    wait(NULL);
+
+    printf("Parent waiting for child 1\n");
+    waitpid(c1, NULL, 0);
+
+    printf("Parent waiting for child 2\n");
+    waitpid(c2, NULL, 0);
+
+    printf("All Childern finished\n");
+
+    return 0;
+}
