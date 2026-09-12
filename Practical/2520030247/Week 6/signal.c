79f513f6f680705cd91d15adb9cef80b3efcf88a
vr4708603-svg <vr4708603@gmail.com>
2026-09-12 08:56:09 +0530

    Add signal handling in signal.c
    Implement signal handling for SIGINT, SIGTERM, and SIGUSR1.

--- Practical/2520030247/Week 6/signal.c
+++ Practical/2520030247/Week 6/signal.c
@@ -1,1 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
 
+void handler(int sig)
+{
+    if (sig == SIGINT)
+        printf("\nSIGINT received\n");
+
+    else if (sig == SIGTERM)
+        printf("\nSIGTERM received\n");
+
+    else if (sig == SIGUSR1)
+        printf("\nSIGUSR1 received\n");
+}
+
+int main()
+{
+    struct sigaction action;
+
+    action.sa_handler = handler;
+    sigemptyset(&action.sa_mask);
+    action.sa_flags = 0;
+
+    sigaction(SIGINT, &action, NULL);
+    sigaction(SIGTERM, &action, NULL);
+    sigaction(SIGUSR1, &action, NULL);
+
+    printf("Process running...\n");
+    printf("PID = %d\n", getpid());
+
+    while (1)
+    {
+        printf("Waiting for signals...\n");
+        sleep(3);
+    }
+
+    return 0;
+}
